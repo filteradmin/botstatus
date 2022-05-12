198dc6fe8945895e12bd7fece244206edf7d1b94
SAF ONE <asmsafone@gmail.com>
2022-05-12 18:30:52 +0600

    .

--- main.py
+++ main.py
@@ -26,29 +26,32 @@ async def S1BOTS():
     try:
         load_dotenv("config.env")
         bots = getConfig("BOTS").split()
-        user_bot = TelegramClient(StringSession(getConfig("SESSION")), int(getConfig("APP_ID")), getConfig("API_HASH"))
+        user_bot = TelegramClient(
+            StringSession(getConfig("SESSION")),
+            int(getConfig("APP_ID")),
+            getConfig("API_HASH")
+        )
+        print("[INFO] Starting client...")
+        await user_bot.start()
     except Exception as e:
         print(f"[ERROR] {str(e)}")
-
-    async with user_bot:
-        print("[INFO] Starting client...")
+    try:
+        await user_bot.edit_message(
+            int(getConfig("CHANNEL_ID")),
+            int(getConfig("MESSAGE_ID")),
+            "**Our Bot's 🤖 Status 📈 :**\n\n`Performing a periodic check...`",
+        )
+    except MessageNotModifiedError:
+        pass
+    c = 0
+    edit_text = "**Our Bot's 🤖 Status 📈 :**\n(Updating Every 30 Minutes)\n\n"
+    print("[INFO] Starting to check uptime...")
+    for bot in bots:
         try:
-            await user_bot.edit_message(
-                int(getConfig("CHANNEL_ID")),
-                int(getConfig("MESSAGE_ID")),
-                "**Our Bot's 🤖 Status 📈 :**\n\n`Performing a periodic check...`",
-            )
-        except MessageNotModifiedError:
-            pass
-        c = 0
-        edit_text = "**Our Bot's 🤖 Status 📈 :**\n(Updating Every 30 Minutes)\n\n"
-        print("[INFO] Starting to check uptime...")
-        for bot in bots:
-            try:
-                print(f"[INFO] checking @{bot}")
-                snt = await user_bot.send_message(bot, "/start")
-                await asyncio.sleep(10)
-                history = await user_bot(
+            print(f"[INFO] checking @{bot}")
+            snt = await user_bot.send_message(bot, "/start")
+            await asyncio.sleep(10)
+            history = await user_bot(
                     GetHistoryRequest(
                         peer=bot,
                         offset_id=0,
@@ -60,26 +63,26 @@ async def S1BOTS():
                         hash=0,
                     )
                 )
-                msg = history.messages[0].id
-                if snt.id == msg:
-                    print(f"[WARNING] @{bot} is down.")
-                    edit_text += f"🤖 **@{bot}** → ❌\n"
-                elif snt.id + 1 == msg:
-                    edit_text += f"🤖 **@{bot}** → ✅\n"
-                c += 1
-                await user_bot.send_read_acknowledge(bot)
-                await user_bot.edit_message(int(getConfig("CHANNEL_ID")), int(getConfig("MESSAGE_ID")), edit_text)
-            except MessageNotModifiedError:
-                pass
-            except FloodWaitError as f:
-                print(f"[WARNING] Floodwait, sleeping for {f.seconds}...")
-                sleep(f.seconds + 10)
+            msg = history.messages[0].id
+            if snt.id == msg:
+                print(f"[WARNING] @{bot} is down.")
+                edit_text += f"🤖 **@{bot}** → ❌\n"
+            elif snt.id + 1 == msg:
+                edit_text += f"🤖 **@{bot}** → ✅\n"
+            c += 1
+            await user_bot.send_read_acknowledge(bot)
+        except MessageNotModifiedError:
+            pass
+        except FloodWaitError as f:
+            print(f"[WARNING] Floodwait, sleeping for {f.seconds}...")
+            sleep(f.seconds + 10)
 
-        tz = pytz.timezone("Asia/Dhaka")
-        edit_text += f"\n**Last Checked ⏳ On** :"
-        print(f"[INFO] Checks since last restart - {c}")
-        edit_text += f"\n`{dt.now(tz).strftime('%d %B %Y')} - {dt.now(tz).strftime('%H:%M:%S')} [BST]`"
-        await user_bot.edit_message(int(getConfig("CHANNEL_ID")), int(getConfig("MESSAGE_ID")), edit_text)
+    tz = pytz.timezone("Asia/Dhaka")
+    edit_text += f"\n**Last Checked ⏳ On** :"
+    print(f"[INFO] Checks since last restart - {c}")
+    edit_text += f"\n`{dt.now(tz).strftime('%d %B %Y')} - {dt.now(tz).strftime('%H:%M:%S')} [BST]`"
+    await user_bot.edit_message(int(getConfig("CHANNEL_ID")), int(getConfig("MESSAGE_ID")), edit_text)
+    return os.system(f"kill -9 {os.getpid()} && python3 main.py")
 
 
 if __name__ == "__main__":
